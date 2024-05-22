5cd4f96ff4ca60b702e1639aea52c4f68b586599
stefasmb <lolboss123lol@gmail.com>
2024-05-22 18:44:08 +0300

    welcome page

--- PixelPower2024/app/src/main/java/com/example/pixelpower2024/MainActivity.java
+++ PixelPower2024/app/src/main/java/com/example/pixelpower2024/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run()
             {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
                 startActivity(intent);
                 finish();
             }
