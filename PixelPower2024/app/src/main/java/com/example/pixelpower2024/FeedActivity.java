ee6295157098b9e696b103e5e24700629591d39f
stefasmb <lolboss123lol@gmail.com>
2024-05-23 19:16:33 +0300

    ong si event bbtn

--- PixelPower2024/app/src/main/java/com/example/pixelpower2024/FeedActivity.java
+++ PixelPower2024/app/src/main/java/com/example/pixelpower2024/FeedActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FeedActivity extends AppCompatActivity {
 
+
     GridView gridView;
+
+    Button ongButton;
+    Button eventButton;
     String[] orgNames = {"Org 1", "Org 2", "Org 3", "Org 4", "Org 5"};
     int[] orgImages = {R.drawable.org1, R.drawable.org2, R.drawable.org3, R.drawable.org4, R.drawable.org5};
 
@@ -17,6 +22,8 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        ongButton = findViewById(R.id.ongButton);
+        eventButton = findViewById(R.id.eventButton);
 
         gridView = findViewById(R.id.gridView);
         GridAdapter adapter = new GridAdapter(this, orgNames, orgImages);
@@ -31,5 +38,20 @@ public class FeedActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FeedActivity.this, OngActivty.class);
+                startActivity(intent);
+            }
+        });
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FeedActivity.this, EventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
