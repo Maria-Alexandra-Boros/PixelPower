01364bf13305fb6b599d861715c815b491bd4032
Maria Alexandra Boros <boros.maria.alexandra@hotmail.com>
2024-05-23 19:22:07 +0300

    Profile button

--- PixelPower2024/app/src/main/java/com/example/pixelpower2024/FeedActivity.java
+++ PixelPower2024/app/src/main/java/com/example/pixelpower2024/FeedActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FeedActivity extends AppCompatActivity {
 
     GridView gridView;
+    ImageButton profileButton;
     String[] orgNames = {"Org 1", "Org 2", "Org 3", "Org 4", "Org 5"};
     int[] orgImages = {R.drawable.org1, R.drawable.org2, R.drawable.org3, R.drawable.org4, R.drawable.org5};
 
@@ -18,16 +22,29 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+        profileButton = findViewById(R.id.profileButton);
         gridView = findViewById(R.id.gridView);
         GridAdapter adapter = new GridAdapter(this, orgNames, orgImages);
         gridView.setAdapter(adapter);
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(FeedActivity.this, DetailsActivity.class);
                 intent.putExtra("name", orgNames[position]);
                 intent.putExtra("image", orgImages[position]);
+                startActivity(intent);
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View view)
+            {
+                Intent intent = new Intent(FeedActivity.this, EditProfileActivity.class);
                 startActivity(intent);
             }
         });
