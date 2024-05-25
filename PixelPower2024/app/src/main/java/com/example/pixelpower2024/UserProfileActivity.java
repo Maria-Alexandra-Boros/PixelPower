b00315bc9a03849a8b77c78d1e962245dbbe2cde
stefasmb <lolboss123lol@gmail.com>
2024-05-25 23:02:58 +0300

    bug fix

--- PixelPower2024/app/src/main/java/com/example/pixelpower2024/UserProfileActivity.java
+++ PixelPower2024/app/src/main/java/com/example/pixelpower2024/UserProfileActivity.java
@@ -12,14 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UserProfileActivity extends AppCompatActivity {
-
-
     Button editProfileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_profile);
+        editProfileButton=findViewById(R.id.editProfileButton);
 
         editProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
